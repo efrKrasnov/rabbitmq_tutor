8c23feb1453932ac6f8049eeadca088a96a9d68c
efr_kras <efrKras@gmail.com>
2021-07-07 20:26:46 +0300

    now we can filter messages by multiple criteria

--- emit_log.py
+++ emit_log.py
@@ -4,10 +4,11 @@ import sys
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='direct_logs', exchange_type='direct')
+channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
 
-severity = sys.argv[1] if len(sys.argv) > 1 else 'info'
+routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
 message = ' '.join(sys.argv[2:]) or "Hello World!"
-channel.basic_publish(exchange='direct_logs', routing_key=severity, body=message)
-print("[x] Sent %r%r" % (severity, message))
+
+channel.basic_publish(exchange='topic_logs', routing_key=routing_key, body=message)
+print("[x] Sent %r%r" % (routing_key, message))
 connection.close()
